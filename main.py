080b940c19700b9f3ed8ee6fdce682b9426804a2
NikZprojects <NikZprojects@gmail.com>
2020-08-20 01:44:32 -0400

    Added new logo! Added menu bar links, bg-clor is darker

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/about")
+def about_redirect():
+    return redirect("/about/", code=302)
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
 @app.route("/recipes")
 def all_recipes_redirect():
     return redirect("/recipes/", code=302)
